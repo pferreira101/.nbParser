8a485de10415c9a52b1d1890b7febc0802d7bf6a
hpereira98 <pereirapalmeirahenrique@gmail.com>
2018-05-06 17:22:08 +0100

    leitura simples dos comandos do ficheiro .nb

--- src/main.c
+++ src/main.c
@@ -2,16 +2,48 @@
 #include <unistd.h> //read/write
 #include <stdio.h> //printf
 #include <stdlib.h> //malloc
+#include <string.h> //strings
+#include <sys/stat.h>
+
+/* PROBLEMA QUE ME SURGIU: RELER OUTRA VEZ APÓS A EXECUÇÃO DO PRIMEIRO COMANDO?? */
 
 int main () {
+
+	char *path = "exemplo.nb";	
+	char *buffer;
 	
-	char *buffer=malloc(sizeof(char)*1024);
+	int file = open(path,O_RDWR,0666); //abrir ficheiro
+	int fileSize;
 
-	int file = open("exemplo.nb",O_RDWR,0666); //abrir ficheiro	
+	if (file>0) { 
+		struct stat st; // ver tamanho do ficheiro
+		if (stat(path,&st)!=-1) {
+			fileSize=st.st_size; //em bytes
+			printf("ficheiro: %d bytes\n",fileSize);
+			buffer=malloc(fileSize);
+		}
+		
+		while(read(file,buffer,fileSize)>0) // carregar ficheiro para string Buffer
+			printf("%s\n",buffer);
 
-	if (file>0) {
-		while (read(file,buffer,1024)>0)
-			printf("%s\n",buffer); //teste leitura
+		for (int i=0;i<fileSize;i++) {
+			int lidos=0;
+			if (buffer[i]=='$') { //carregar comando simples para a string cmd
+				int j;				
+				if (buffer[i+1]=='|') j=i+3;
+				else j=i+2;
+
+				char cmd[2048]; //max unix line: getconf LINE_MAX
+				for (;j<fileSize && buffer[j]!='\n';j++) {					
+					memset(cmd+lidos,buffer[j],1);
+					lidos++;
+				}
+				printf("%s\n",cmd);
+
+				execlp(cmd,cmd,NULL); // precisa de um fork
+			}
+		}
+
 		close(file);	
 	}
 
